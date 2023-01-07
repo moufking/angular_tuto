746984c7142859784dbabac92ba8c0ed2b739d7b
moufking <moufking337@gmail.com>
2023-01-07 19:01:34 +0100

    add describe component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tuto';
+  title = 'Ceci est notre premier composant';
 }
